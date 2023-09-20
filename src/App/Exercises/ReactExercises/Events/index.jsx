8020769bba118b26eadd6e4cac0dd51d42b6dda9
Irmina Kalinowska <irminakalinowska0@gmail.com>
2023-09-20 21:03:32 +0200

    excercices react Events

--- src/App/Exercises/ReactExercises/Events/index.jsx
+++ src/App/Exercises/ReactExercises/Events/index.jsx
@@ -4,14 +4,40 @@ import './style.css';
 
 export function Events() {
   const [text, setText] = useState('Nie kliknięto we mnie');
+  const [counter, setCounter] = useState(0);
+  const [inputText, setInputText] = useState('');
+
   const handleOnClick = () => {
     setText('Kliknięto we mnie');
+    counter > 3
+      ? setCounter((counter) => counter + 2)
+      : setCounter((counter) => counter + 1);
+  };
+
+  function handleOnChange(event){
+    setInputText(event.target.value);
+  };
+
+  const greeting = () => {
+    alert(`Czy miałeś na myśli ${inputText}?`)
   };
 
   return (
     <div>
-      <h2>Cześć </h2>
-      <button onClick={handleOnClick}>{text}</button>
+      <h2>Cześć! </h2>
+      <p>Wpisz tekst</p>
+      <input
+        onChange={(event) => handleOnChange(event)}
+        type="text"
+        placeholder="tekst do wpisania"
+      />
+      <div>
+        <button onClick={greeting}>Enter</button>
+        <button onClick={handleOnClick}>
+          {/* <span>{text}</span> {counter > 0 && <span>{counter} razy</span>} */}
+          {counter > 0 ? `${text} ${counter} razy` : text}
+        </button>
+      </div>
     </div>
   );
 }
