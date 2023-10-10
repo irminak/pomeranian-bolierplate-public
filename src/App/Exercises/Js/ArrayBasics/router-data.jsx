d9a7fc9731ac271e9cb1a6ff93cc0603a4cf46ad
Irmina Kalinowska <irminakalinowska0@gmail.com>
2023-10-10 20:34:45 +0200

    arrays

---
+++ src/App/Exercises/Js/ArrayBasics/router-data.jsx
@@ -0,0 +1,10 @@
+import { ArrayBasics } from './index';
+
+export const blockRouterMetaData = {
+  path: 'arrays-js-basics',
+  date: '10.10.2023',
+  linkLabel: 'Metody na tablicach 10.10.2023',
+  blockNo: 21,
+  element: <ArrayBasics />,
+  tags: ['arrays', 'javascript'],
+};
