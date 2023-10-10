ea67c8fd018c56b6b4277d2fee433dd59e09d8a8
Irmina Kalinowska <irminakalinowska0@gmail.com>
2023-10-10 16:59:24 +0200

    arrays js

---
+++ src/App/Exercises/Js/StringMethods/router-data.jsx
@@ -0,0 +1,15 @@
+import { StringMethods } from './index';
+
+export const blockRouterMetaData = {
+  path: 'string-methods',
+
+  date: '06-10-2023',
+
+  linkLabel: 'Metody na stringach',
+
+  blockNo: 19,
+
+  element: <StringMethods />,
+
+  tags: ['null', 'undefined', 'string'],
+};
