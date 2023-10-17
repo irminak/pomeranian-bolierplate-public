2b5d235a2017357a5fa5f9d6dc8b0cc7fc3c3775
Irmina <144358461+irminak@users.noreply.github.com>
2023-10-17 08:16:07 +0200

    Feat/hit the mole (#21)
    * hit the mole class
    
    * mole game style

---
+++ src/App/Exercises/ReactExercises/HitTheMole2/router-data.jsx
@@ -0,0 +1,10 @@
+import { HitTheMole2 } from './index';
+
+export const blockRouterMetaData = {
+  path: 'hit-the-molee',
+  date: '13-10-2023',
+  linkLabel: 'Uderz w kretaa2',
+  blockNo: 21,
+  element: <HitTheMole2 />,
+  tags: ['components', 'setInterval', 'map', 'useState'],
+};
