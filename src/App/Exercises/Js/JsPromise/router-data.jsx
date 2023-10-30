2005bd87740acc19c98aec4ff94fc49e87b8f202
Irmina <144358461+irminak@users.noreply.github.com>
2023-10-30 17:09:22 +0100

    promises (#31)

---
+++ src/App/Exercises/Js/JsPromise/router-data.jsx
@@ -0,0 +1,21 @@
+import { JsPromise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-promise',
+  date: '27-10-2023',
+  linkLabel: 'JS - Promise',
+  blockNo: 34,
+  element: <JsPromise />,
+  tags: [
+    'js',
+    'promise',
+    'resolve',
+    'reject',
+    'then',
+    'catch',
+    'finally',
+    'pending',
+    'fullfiled',
+    'rejected',
+  ],
+};
---
+++ src/App/Exercises/Js/JsPromiseSymulacjaAPI/router-data.jsx
@@ -0,0 +1,21 @@
+import { JsPromiseSymulacjaAPI } from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-promise-api',
+  date: '27-10-2023',
+  linkLabel: 'JS - Promise API',
+  blockNo: 34,
+  element: <JsPromiseSymulacjaAPI />,
+  tags: [
+    'js',
+    'promise',
+    'resolve',
+    'reject',
+    'then',
+    'catch',
+    'finally',
+    'pending',
+    'fullfiled',
+    'rejected',
+  ],
+};
